b45fbd46b55538745a2cc361b1d7d13b480dc8e7
Mohamed Bifenzi <mbifenzi@e2r2p9.1337.ma>
2021-12-03 13:51:04 +0100

    fistone

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: mbifenzi <mbifenzi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 18:12:34 by mbifenzi          #+#    #+#             */
-/*   Updated: 2021/11/14 23:11:52 by mbifenzi         ###   ########.fr       */
+/*   Updated: 2021/12/03 13:25:40 by mbifenzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,21 @@ int	error(char *error)
 }
 int	ft_protection(t_data *args, int argc, char **argv)
 {
-    if (argc != 7 && argc != 6)
+    if (argc != 5 && argc != 6)
         return (error("INVALID ARGUMENTS\n"));
-	if ( argv[1] < 0 || argv[2] < 0 || argv[3] < 0 || argv[4] < 0 || argv[5] < 0)
-		return (error("negative something\n"));
 	args->philos = ft_atoi(argv[1]);
 	args->die = ft_atoi(argv[2]);
 	args->eat = ft_atoi(argv[3]);
 	args->sleep = ft_atoi(argv[4]);
+	if ( args->philos < 0 || args->die < 0 || args->eat < 0 || args->sleep < 0)
+		return (error("negative something\n"));
 	if (argc == 6)
+	{		
 		args->meals = ft_atoi(argv[5]);
-		return(1);
+		if (args->meals <= 0)
+			return (error("negative something\n"));
+	}		
+	return(1);
 }
 
 int main(int argc, char **argv)
@@ -38,5 +42,7 @@ int main(int argc, char **argv)
 	args = malloc(sizeof(t_data));
 	if (!ft_protection(args, argc, argv))
 		exit (0);
+	
+	
 
 }
